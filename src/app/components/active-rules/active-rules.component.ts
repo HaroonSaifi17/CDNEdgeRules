aded777b525ba9af781c532990fa1da4ba14b90e
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-11-28 20:09:12 +0530

    update

--- src/app/components/active-rules/active-rules.component.ts
+++ src/app/components/active-rules/active-rules.component.ts
@@ -3,22 +3,39 @@ import { EdgeRulesDataService } from '../../utils/edge-rules-data.service';
 import { ActivatedRoute } from '@angular/router';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { SnakeCasePipe } from '../../utils/snake-case.pipe';
+import { FormsModule } from '@angular/forms';
+import { RippleModule } from 'primeng/ripple';
+import { Rule } from '../../utils/EdgeRule.interface';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-active-rules',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, SnakeCasePipe],
+  imports: [CommonModule, AsyncPipe, SnakeCasePipe, FormsModule, RippleModule],
   templateUrl: './active-rules.component.html',
   styleUrl: './active-rules.component.scss',
 })
 export class ActiveRulesComponent {
-  activeRules$ = inject(EdgeRulesDataService).activeRules$;
+  activeRule$ = inject(EdgeRulesDataService).activeRules$.pipe(
+    map((rules) =>
+      rules.find(
+        (rule) =>
+          new SnakeCasePipe().transform(rule.name) ===
+          this.tabParam
+      ),
+    ),
+  );
+
   private route = inject(ActivatedRoute);
   tabParam: string | null = null;
+  activeRule: Rule | undefined;
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.tabParam = params['tab'];
+    });
+    this.activeRule$.subscribe((activeRule) => {
+      this.activeRule = activeRule;
     });
   }
 }
--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -3,11 +3,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { EdgeRulesDataService } from '../../utils/edge-rules-data.service';
 import { SnakeCasePipe } from '../../utils/snake-case.pipe';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [RouterLink, CommonModule, SnakeCasePipe, RouterLinkActive],
+  imports: [RouterLink, CommonModule, SnakeCasePipe, RouterLinkActive, RippleModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
